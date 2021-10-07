e27fd148d8b3d4c232de8fb3c847b76aae312edd
Iuri Almeida <ialmeida@vertigo.com.br>
2021-10-07 17:03:32 -0300

    Local porject created - Changing local setup

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,21 +1,27 @@
 package application;
-
+	
 import java.io.IOException;
 
+import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main {
 
+public class Main extends Application {
+	
+	@Override
 	public void start(Stage stage) {
 		
 		try {
 			
-			Parent parent = FXMLLoader.load(getClass().getResource("/gui/View.fxml"));
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
+			Parent parent = loader.load();
+			
 			Scene scene = new Scene(parent);
 			stage.setScene(scene);
+			stage.setTitle("Sample JavaFX application");
 			stage.show();
 			
 		} catch(IOException e) {
@@ -25,7 +31,6 @@ public class Main {
 	}
 	
 	public static void main(String[] args) {
-		System.out.println("teste");
+		launch(args);
 	}
-
 }
